b223267faf893b2446275666a3a7acfa70dee22a
RomanStar228 <nastyastar@gmail.com>
2025-10-29 15:05:18 +0500

    index

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <a href=""><h1>ЛАБУБУ.МАРКЕТ</h1></a>
+    </header>
+    <main>
+        <div class="container">
+            <form method="POST" action="">
+                <div class="cards">
+                    
+                </div>
+            </form>
+        </div>
+
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
